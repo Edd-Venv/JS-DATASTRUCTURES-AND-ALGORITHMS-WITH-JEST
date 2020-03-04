f0fc045052aaee6481729283e092b2653886d50e
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-03-04 08:47:14 +0100

    BetterHashing

--- Jest-Testing-Algorithms/Data-Structures-With-Jest/BetterHashing.test.js
+++ Jest-Testing-Algorithms/Data-Structures-With-Jest/BetterHashing.test.js
@@ -13,22 +13,37 @@ class BetterHashTable {
 
   betterHash(data) {
     const primeNumber = 37;
-    const str = data.toUpperCase();
-    let total = 0;
-    for (let i = 0; i < str.length; i++) {
-      total += primeNumber * total + str.charCodeAt(i);
+    if (typeof data === "string") {
+      const str = data.toUpperCase();
+      let total = 0;
+      for (let i = 0; i < str.length; i++) {
+        total += primeNumber * total + str.charCodeAt(i);
+      }
+      total = total % this.table.length;
+      if (total < 0) {
+        total += this.table.length - 1;
+      }
+      return parseInt(total);
+    } else if (typeof data === "number") {
+      let total = 0;
+      total += primeNumber * total + data;
+      total = total % this.table.length;
+      if (total < 0) {
+        total += this.table.length - 1;
+      }
+      return parseInt(total);
     }
-    total = total % this.table.length;
-    if (total < 0) {
-      total += this.table.length - 1;
-    }
-    return parseInt(total);
   }
 
   put(data) {
-    const str = data.toUpperCase();
-    const position = this.betterHash(str);
-    this.table[position] = str;
+    if (typeof data === "string") {
+      const str = data.toUpperCase();
+      const position = this.betterHash(str);
+      this.table[position] = str;
+    } else if (typeof data === "number") {
+      const position = this.betterHash(data);
+      this.table[position] = data;
+    }
   }
 
   showTable() {
@@ -73,7 +88,7 @@ describe("BETTER HASHING", () => {
     );
   });
 
-  it("SHOWTABLE()", () => {
+  it("SHOWTABLE(), Using Console.log()", () => {
     const hashTable = new BetterHashTable();
     const someNames = [
       "David",
@@ -84,12 +99,60 @@ describe("BETTER HASHING", () => {
       "Mike",
       "Clayton",
       "Danny",
-      "Jonathan"
+      "Jonathan",
+      444
     ];
 
     for (let i = 0; i < someNames.length; ++i) {
       hashTable.put(someNames[i]);
     }
-    //console.log(hashTable.showTable());
+    console.log(hashTable.showTable());
   });
 });
+
+//                                SHOWING THAT BETTERHASH IS A BETTER HASHING FUNCTION
+//                              HASHING INTEGER KEYS
+
+/*
+The getRandomInt() function allows us to specify a maximum and minimum random
+number, for a set of student grades.
+*/
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+/*
+The getStuData() function generates student data. The inner loop generates the student
+ID number, and right after the inner loop finishes, a random grade is generated
+and concatenated to the student ID.
+
+The main program will separate the ID from the
+grade. The hash function will total the individual digits in the student ID to compute a
+hash value using the betterHash() function.
+*/
+function generateStudentData(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let num = "";
+    for (let j = 1; j <= 9; j++) {
+      num += Math.floor(Math.random() * 10);
+    }
+    num += getRandomInt(50, 100);
+    arr[i] = num;
+  }
+}
+
+const numOfStudents = 10;
+const arrSize = 97;
+const idLength = 9;
+const students = new Array(numOfStudents);
+generateStudentData(students);
+//console.log("Student data: \n");
+for (let i = 0; i < students.length; i++) {
+  //console.log(students[i].substring(0, 8) + " " + students[i].substring(9));
+}
+//console.log("\n\nData distribution: \n");
+var hTable = new BetterHashTable();
+for (let i = 0; i < students.length; i++) {
+  hTable.put(students[i]);
+}
+//console.log(hTable.showTable());
