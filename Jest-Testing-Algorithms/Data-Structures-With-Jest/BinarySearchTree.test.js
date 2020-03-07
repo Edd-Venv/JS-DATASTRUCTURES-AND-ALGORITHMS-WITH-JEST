755491f8805e5bf8c74dd2c941e13c37b9b71464
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-03-07 16:42:03 +0100

    BINARY SEARCH TREE COMPLETE

--- Jest-Testing-Algorithms/Data-Structures-With-Jest/BinarySearchTree.test.js
+++ Jest-Testing-Algorithms/Data-Structures-With-Jest/BinarySearchTree.test.js
@@ -23,8 +23,12 @@ class BinarySearchTree {
     this.findMax;
     this.remove;
     this.findMin;
+    this.findMax;
     this.findMinHeight;
     this.findMaxHeight;
+    this.preOrder;
+    this.postOrder;
+    this.levelOrder;
   }
 
   insert(data) {
@@ -34,6 +38,10 @@ class BinarySearchTree {
     } else {
       let current = this.root;
       let parent;
+      //Next Check Should Be Removed IG You Want To Use BST For Number Of Occurrences And A Counter Added To The Node Object
+      if (data === current.data) {
+        return null;
+      }
       while (true) {
         parent = current;
         if (data < current.data) {
@@ -65,6 +73,57 @@ class BinarySearchTree {
       }
       traverseInOrder(this.root);
       return result;
+    }
+  }
+
+  preOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePreOrder(node) {
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      }
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+
+  postOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePostOrder(node) {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
+
+  levelOrder() {
+    let result = [];
+    let Q = [];
+    if (this.root !== null) {
+      Q.push(this.root);
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+        if (node.left !== null) {
+          Q.push(node.left);
+        }
+        if (node.right !== null) {
+          Q.push(node.right);
+        }
+      }
+      return result;
+    } else {
+      return null;
     }
   }
 
@@ -183,19 +242,9 @@ describe("BINARY-SEARCH-TREE", () => {
   it("INSERT(), Should Add A New Node, Either Left Or Right Of The BSTree, Depending On The Value Of The Data", () => {
     const BST = new BinarySearchTree();
     BST.insert(2);
+    BST.insert(2);
     expect(BST.root).toMatchObject({ data: 2, left: null, right: null });
-  });
-
-  it("INORDER(), Should Traversal Visits All The Nodes Of A BST", () => {
-    const BST = new BinarySearchTree();
-    BST.insert(23);
-    BST.insert(45);
-    BST.insert(16);
-    BST.insert(37);
-    BST.insert(3);
-    BST.insert(99);
-    BST.insert(22);
-    expect(BST.inOrder(BST.root)).toEqual([3, 16, 22, 23, 37, 45, 99]);
+    expect(BST.insert(2)).toBeNull();
   });
 
   it("FINDMAX(), Should Find The Max Data On The Right Child Of The Root", () => {
@@ -237,24 +286,26 @@ describe("BINARY-SEARCH-TREE", () => {
     expect(BST.findMin()).toBe(10);
   });
 
-  it("FINDMINHEIGHT(), Should Find The Minimum Level", () => {
+  it("FINDMINHEIGHT(), Should Find The Distance(Level) From The Root Node To The First Leaf Node WithOut Two Children", () => {
     const BST = new BinarySearchTree();
     BST.insert(23);
     BST.insert(45);
     BST.insert(16);
     BST.insert(37);
     BST.insert(10);
+    BST.insert(47);
     expect(BST.findMinHeight()).toBe(1);
   });
 
-  it("FINDMAXHEIGHT(), Should Find The Minimum Level", () => {
+  it("FINDMAXHEIGHT(), Should The Distance From The Root Node To The Most Bottom Node", () => {
     const BST = new BinarySearchTree();
     BST.insert(23);
     BST.insert(45);
     BST.insert(16);
     BST.insert(37);
     BST.insert(10);
-    expect(BST.findMaxHeight()).toBe(2);
+    BST.insert(43);
+    expect(BST.findMaxHeight()).toBe(3);
   });
 
   it("ISPRESENT(), Should Cheak If A Node Exists In The Tree", () => {
@@ -279,5 +330,67 @@ describe("BINARY-SEARCH-TREE", () => {
     expect(BST.find(16)).toBeFalsy();
     expect(BST.isPresent(16)).toBeFalsy();
     expect(BST.findMin()).toBe(23);
+  });
+
+  it("INORDER(), Should Visit All The Nodes Of A BST in Ascending Order Of The Node Key Values(Data).", () => {
+    const BST = new BinarySearchTree();
+    BST.insert(23);
+    BST.insert(45);
+    BST.insert(16);
+    BST.insert(37);
+    BST.insert(3);
+    BST.insert(99);
+    BST.insert(22);
+    expect(BST.inOrder(BST.root)).toEqual([3, 16, 22, 23, 37, 45, 99]);
+  });
+
+  it("PREORDER(), Should Visit The Root Nood Fisrt Then Sub Trees Of The Left Child Of The Root Node Then The Subtrees Of The Right Node Last.", () => {
+    const BST = new BinarySearchTree();
+    BST.insert(23);
+    BST.insert(45);
+    BST.insert(16);
+    BST.insert(37);
+    BST.insert(3);
+    BST.insert(99);
+    BST.insert(22);
+    expect(BST.preOrder()).toEqual([23, 16, 3, 22, 45, 37, 99]);
+  });
+
+  it("POSTORDER(), Should Visit All The Child Nodes Of The Left Subtree Up To The Root Node, Then Does The Same For The Right Subtree", () => {
+    const BST = new BinarySearchTree();
+    BST.insert(23);
+    BST.insert(45);
+    BST.insert(16);
+    BST.insert(37);
+    BST.insert(3);
+    BST.insert(99);
+    BST.insert(22);
+    expect(BST.postOrder()).toEqual([3, 22, 16, 37, 99, 45, 23]);
+  });
+
+  it("LEVELORDER(), Should Be Visit Every Level Starting From The Root Then Root Left Child Then The Root Right Child", () => {
+    const BST = new BinarySearchTree();
+    BST.insert(9);
+    BST.insert(4);
+    BST.insert(17);
+    BST.insert(3);
+    BST.insert(6);
+    BST.insert(22);
+    BST.insert(5);
+    BST.insert(7);
+    BST.insert(20);
+    BST.insert(10);
+    expect(BST.levelOrder()).toEqual([9, 4, 17, 3, 6, 10, 22, 5, 7, 20]);
+  });
+
+  it("ISBALANCED(), Should Be Truthy If At Most The Differnece Is By One", () => {
+    const BST = new BinarySearchTree();
+    BST.insert(23);
+    BST.insert(45);
+    BST.insert(16);
+    BST.insert(37);
+    expect(BST.isBalanced()).toBeTruthy();
+    BST.insert(40);
+    expect(BST.isBalanced()).toBeFalsy();
   });
 });
